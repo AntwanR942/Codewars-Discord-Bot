ce185f1c58c90919a613857bcb1bf7c652c1544a
AntwanR942 <antwang942@gmail.com>
2021-02-14 21:49:35 +0000

    add github link to info command

--- modules/CW-Me.lua
+++ modules/CW-Me.lua
@@ -5,7 +5,7 @@ CommandManager.Command("info", function(Args, Payload)
 
         I made this bot in my spare time as a fun side project since I enjoy Codewars and making Discord bots.
 
-        This bot is completely open source and can be found on my GitHub [here]() along with my other programming ventures.
+        This bot is completely open source and can be found on my GitHub [here](https://github.com/AntwanR942/Codewars-Discord-Bot) along with my other programming ventures.
 
         If you feel generous and want to support me and my work then you can [buy me a coffee :coffee:](https://www.buymeacoffee.com/AntwanR942).
 
